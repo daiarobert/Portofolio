523beac4bf63a5a306248729ec0ef4cba00c4e89
Robert Daia <RobertDaia@insideideas.co.uk>
2023-01-23 23:00:33 +0000

    wip

--- src/js/model.js
+++ src/js/model.js
@@ -1,8 +1,8 @@
 var dataModel = {
   1: {
-    apiEndpoint:
-      "https://api.channelsight.com/api/2.0/feed?key=b0d332b1-2100-40ad-89e1-c696c042a3c4&assetId=3686",
-    name: '<div class="inline-nav__text--1">test-1</div>',
+    // apiEndpoint:
+    //   "https://api.channelsight.com/api/2.0/feed?key=b0d332b1-2100-40ad-89e1-c696c042a3c4&assetId=3686",
+    // name: '<div class="inline-nav__text--1">test-1</div>',
     retailers: {},
     ui: {
       retailers: {},
